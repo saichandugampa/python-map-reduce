1848ca903e5f1922dd266382a1c9c87b3faee450
saichandugampa <gampa.saichandu95@gmail.com>
2020-05-26 12:56:52 -0500

    Changed output file names.

--- 13reducer.py
+++ 13reducer.py
@@ -2,8 +2,8 @@
 # Easy to test
 # Not quite Hadoop-ready
 
-with open("output12sorted.txt","r") as sorted:
-  with open("output13reduced.txt", "w") as output:
+with open("gampasorted.txt","r") as sorted:
+  with open("saireduced.txt", "w") as output:
 
     thisKey = ""
     thisValue = 0.0
